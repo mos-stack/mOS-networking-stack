mos-stack/mOS-networking-stack

commit 05e3289c3af6e14b7feea755af0809a609874c48

Patches for mOS release 0.4 (see http://mos.kaist.edu/guide/release/04_release_note.html#mos-release-0-4-october-21-2017 for more details)

samples/nat/nat.c
@@ -64,7 +64,7 @@ assign_port(mctx_t mctx, int sock)
 
 	/* remove a NAT mapping for this connection */
 	if (mtcp_getpeername(mctx, sock, (struct sockaddr *)&addr, &len,
-						 MOS_SIDE_CLI) < 0) {
+						 MOS_SIDE_BOTH) < 0) {
 		TRACE_ERROR("mtcp_getpeer() failed for sock=%d\n", sock);
 		return;
 	}
@@ -140,7 +140,7 @@ translate_addr(mctx_t mctx, int sock, int side, uint64_t events,
 		socklen_t len = sizeof(addr) * 2;
 
 		if (mtcp_getpeername(mctx, sock, (struct sockaddr *)&addr, &len,
-					MOS_SIDE_CLI) < 0) {
+					MOS_SIDE_BOTH) < 0) {
 			TRACE_ERROR("mtcp_getpeer() failed sock=%d side=%d\n", sock, side);
 			return;
 		}

core/src/core.c
@@ -71,6 +71,7 @@ static int running[MAX_CPUS] = {0};
 mtcp_sighandler_t app_signal_handler;
 static int sigint_cnt[MAX_CPUS] = {0};
 static struct timespec sigint_ts[MAX_CPUS];
+void mtcp_free_context(mctx_t mctx);
 /*----------------------------------------------------------------------------*/
 #ifdef NETSTAT
 #if NETSTAT_TOTAL
@@ -79,7 +80,6 @@ static int printer = -1;
 #endif /* ROUND_STAT */
 #endif /* NETSTAT_TOTAL */
 #endif /* NETSTAT */
-void mtcp_free_context(mctx_t mctx);
 /*----------------------------------------------------------------------------*/
 void
 HandleSignal(int signal)
@@ -440,11 +440,19 @@ FlushMonitorReadEvents(mtcp_manager_t mtcp)
 						cur_stream->rcvvar->rcvbuf != NULL) {
 					/* no need to pass pkt context */
 					struct socket_map *walk;
-					SOCKQ_FOREACH_START(walk, &cur_stream->msocks) {
-						HandleCallback(mtcp, MOS_NULL, walk,
-							       cur_stream->side, NULL, 
-							       MOS_ON_CONN_NEW_DATA);
-					} SOCKQ_FOREACH_END;
+					if (cur_stream->side == MOS_SIDE_CLI) {
+						SOCKQ_FOREACH_REVERSE(walk, &cur_stream->msocks) {
+							HandleCallback(mtcp, MOS_NULL, walk,
+								       cur_stream->side, NULL, 
+								       MOS_ON_CONN_NEW_DATA);
+						} SOCKQ_FOREACH_END;
+					} else { /* cur_stream->side == MOS_SIDE_SVR */
+						SOCKQ_FOREACH_START(walk, &cur_stream->msocks) {
+							HandleCallback(mtcp, MOS_NULL, walk,
+								       cur_stream->side, NULL, 
+								       MOS_ON_CONN_NEW_DATA);
+						} SOCKQ_FOREACH_END;
+					}
 				}
 				/* reset the actions now */
 				cur_stream->actions = 0;
@@ -485,10 +493,17 @@ FlushBufferedReadEvents(mtcp_manager_t mtcp)
 					cur_stream->rcvvar->rcvbuf != NULL) {
 				/* no need to pass pkt context */
 				struct socket_map *walk;
-				SOCKQ_FOREACH_START(walk, &cur_stream->msocks) {
-					HandleCallback(mtcp, MOS_NULL, walk, cur_stream->side,
-						       NULL, MOS_ON_CONN_NEW_DATA);
-				} SOCKQ_FOREACH_END;
+				if (cur_stream->side == MOS_SIDE_CLI) {
+					SOCKQ_FOREACH_REVERSE(walk, &cur_stream->msocks) {
+						HandleCallback(mtcp, MOS_NULL, walk, cur_stream->side,
+							       NULL, MOS_ON_CONN_NEW_DATA);
+					} SOCKQ_FOREACH_END;
+				} else { /* cur_stream->side == MOS_SIDE_SVR */
+					SOCKQ_FOREACH_START(walk, &cur_stream->msocks) {
+						HandleCallback(mtcp, MOS_NULL, walk, cur_stream->side,
+							       NULL, MOS_ON_CONN_NEW_DATA);
+					} SOCKQ_FOREACH_END;
+				}
 			}
 		}
 		if (offset >= mtcpq->size)
@@ -888,6 +903,7 @@ RunMainLoop(struct mtcp_thread_context *ctx)
 				pktbuf = mtcp->iom->get_rptr(mtcp->ctx, rx_inf, i, &len);
 				ProcessPacket(mtcp, rx_inf, i, ts, pktbuf, len);
 			}
+
 		}
 		STAT_COUNT(mtcp->runstat.rounds_rx);
 
@@ -1331,13 +1347,21 @@ MTCPRunThread(void *arg)
 	RunMainLoop(ctx);
 	
 	TRACE_DBG("MTCP thread %d finished.\n", ctx->cpu);
-
+	
 	/* signaling mTCP thread is done */
 	sem_post(&g_done_sem[mctx->cpu]);
 	
 	//pthread_exit(NULL);
 	return 0;
 }
+/*----------------------------------------------------------------------------*/
+#ifdef ENABLE_DPDK
+static int MTCPDPDKRunThread(void *arg)
+{
+	MTCPRunThread(arg);
+	return 0;
+}
+#endif /* !ENABLE_DPDK */
 /*----------------------------------------------------------------------------*/
 mctx_t 
 mtcp_create_context(int cpu)
@@ -1394,13 +1418,34 @@ mtcp_create_context(int cpu)
 		TRACE_ERROR("Failed to create log thread\n");
 		return NULL;
 	}
-	
-	if (pthread_create(&g_thread[cpu], 
-			   NULL, MTCPRunThread, (void *)mctx) != 0) {
-		TRACE_ERROR("pthread_create of mtcp thread failed!\n");
-		return NULL;
-	}
-	
+
+	/* use rte_eal_remote_launch() for DPDK
+	   (worker/slave threads are already initialized by rte_eal_init()) */
+#ifdef ENABLE_DPDK
+	/* Wake up mTCP threads (wake up I/O threads) */
+	if (current_iomodule_func == &dpdk_module_func) {
+		int master;
+		master = rte_get_master_lcore();
+		if (master == cpu) {
+			lcore_config[master].ret = 0;
+			lcore_config[master].state = FINISHED;
+			if (pthread_create(&g_thread[cpu], 
+					   NULL, MTCPRunThread, (void *)mctx) != 0) {
+				TRACE_ERROR("pthread_create of mtcp thread failed!\n");
+				return NULL;
+			}
+		} else
+			rte_eal_remote_launch(MTCPDPDKRunThread, mctx, cpu);
+	} else 
+#endif /* !ENABLE_DPDK */
+		{
+			if (pthread_create(&g_thread[cpu], 
+					   NULL, MTCPRunThread, (void *)mctx) != 0) {
+				TRACE_ERROR("pthread_create of mtcp thread failed!\n");
+				return NULL;
+			}
+		}
+
 	sem_wait(&g_init_sem[cpu]);
 	sem_destroy(&g_init_sem[cpu]);
 
@@ -1428,15 +1473,12 @@ mtcp_destroy_context(mctx_t mctx)
 	struct mtcp_context m;
 	m.cpu = mctx->cpu;
 	mtcp_free_context(&m);
-
+	
 	free(mctx);
 
 	return 0;
 }
 /*----------------------------------------------------------------------------*/
-/**
- * TODO: It currently always returns 0. Add appropriate error return values
- */
 void
 mtcp_free_context(mctx_t mctx)
 {
@@ -1448,6 +1490,7 @@ mtcp_free_context(mctx_t mctx)
 	TRACE_DBG("CPU %d: mtcp_free_context()\n", mctx->cpu);
 
 	if (g_pctx[mctx->cpu] == NULL) return;
+
 	/* close all stream sockets that are still open */
 	if (!ctx->exit) {
 		for (i = 0; i < g_config.mos->max_concurrency; i++) {
@@ -1463,16 +1506,23 @@ mtcp_free_context(mctx_t mctx)
 	}
 
 	ctx->done = 1;
-
-	//pthread_kill(g_thread[mctx->cpu], SIGINT);
 	ctx->exit = 1;
 
-	if ((ret = pthread_join(g_thread[ctx->cpu], NULL) != 0)) {
-		TRACE_ERROR("pthread_join() returns error (errno = %s)\n", strerror(ret));
-		exit(EXIT_FAILURE);
-	}
+#ifdef ENABLE_DPDK
+	if (current_iomodule_func == &dpdk_module_func) {
+		int master = rte_get_master_lcore();
+		if (master == mctx->cpu)
+			pthread_join(g_thread[mctx->cpu], NULL);
+		else
+			rte_eal_wait_lcore(mctx->cpu);
+	} else 
+#endif /* !ENABLE_DPDK */
+		{
+			pthread_join(g_thread[mctx->cpu], NULL);
+		}
 	
 	TRACE_INFO("MTCP thread %d joined.\n", mctx->cpu);
+	
 	running[mctx->cpu] = FALSE;
 
 #ifdef NETSTAT
@@ -1494,9 +1544,10 @@ mtcp_free_context(mctx_t mctx)
 		TRACE_ERROR("CPU %d: Fail to signal socket pair\n", mctx->cpu);
 		
 	if ((ret = pthread_join(log_thread[ctx->cpu], NULL) != 0)) {
-		TRACE_ERROR("pthread_join() returns error (errno = %s)\n", strerror(ret));
-		exit(EXIT_FAILURE);
+	    TRACE_ERROR("pthread_join() returns error (errno = %s)\n", strerror(ret));
+		exit(-1);
 	}
+
 
 	fclose(mtcp->log_fp);
 	TRACE_LOG("Log thread %d joined.\n", mctx->cpu);
@@ -1558,7 +1609,6 @@ mtcp_free_context(mctx_t mctx)
 	//TRACE_INFO("MTCP thread %d destroyed.\n", mctx->cpu);
 	if (mtcp->iom->destroy_handle)
 		mtcp->iom->destroy_handle(ctx);
-
 	if (g_logctx[mctx->cpu]) {
 		free(g_logctx[mctx->cpu]);
 		g_logctx[mctx->cpu] = NULL;
@@ -1729,12 +1779,14 @@ mtcp_destroy()
 	int i;
 
 	/* wait until all threads are closed */
+	/*
 	for (i = 0; i < num_cpus; i++) {
 		if (running[i]) {
 			if (pthread_join(g_thread[i], NULL) != 0)
 				return -1;
 		}
 	}
+	*/
 
 	for (i = 0; i < g_config.mos->netdev_table->num; i++)
 		DestroyAddressPool(ap[i]);

core/src/eth_in.c
@@ -29,7 +29,6 @@ ProcessPacket(mtcp_manager_t mtcp, const int ifidx, const int index,
 		uint32_t cur_ts, unsigned char *pkt_data, int len)
 {
 	struct pkt_ctx pctx;
-	struct mon_listener *walk;
 	struct ethhdr *ethh = (struct ethhdr *)pkt_data;
 	int ret = -1;
 	u_short h_proto = ntohs(ethh->h_proto);
@@ -58,12 +57,7 @@ ProcessPacket(mtcp_manager_t mtcp, const int ifidx, const int index,
 		/* process ipv4 packet */
 		ret = ProcessInIPv4Packet(mtcp, &pctx);
 	} else {
-		/* callback for monitor raw socket */
-		TAILQ_FOREACH(walk, &mtcp->monitors, link)
-			if (walk->socket->socktype == MOS_SOCK_MONITOR_RAW)
-				HandleCallback(mtcp, MOS_NULL, walk->socket, MOS_SIDE_BOTH,
-					       &pctx, MOS_ON_PKT_IN);
-		
+
 		/* drop the packet if forwarding is off */
 		if (!mtcp->num_msp || !pctx.forward) {
 #ifdef RUN_ARP

core/src/include/mos_api.h
@@ -527,28 +527,6 @@ int
 mtcp_setlastpkt(mctx_t mctx, int sock, int side, off_t offset,
 		byte *data, uint16_t datalen, int option);
 
-/** Drop current packet (don't forward it to the peer node)
- * @param [in] mctx: mtcp context
- *
- * This is useful for running callback-only applications
- * This function is now deprecated...
- */
-//int
-//mtcp_cb_dropcurpkt(mctx_t mctx);
-
-/* Reset the connection (send RST to both sides)
- * (This API will be updated after discussion.)
- */
-int
-mtcp_reset_conn(mctx_t mctx, int sock);
-
-int
-mtcp_set_debug_string(mtcp_manager_t mtcp, const char *fmt, ...);
-
-int
-mtcp_get_debug_string(mctx_t mctx, char *buf, int len);
-
-/**************************************************************************/
 /** Send a TCP packet of struct pkt_info
  * @param [in] mctx: mTCP/mOS context
  * @param [in] sock: monitoring stream socket id
@@ -559,7 +537,5 @@ mtcp_get_debug_string(mctx_t mctx, char *buf, int len);
  */ 
 int
 mtcp_sendpkt(mctx_t mctx, int sock, const struct pkt_info *pkt);
-
-/**************************************************************************/
 
 #endif /* __MOS_API_H_ */

core/src/include/mtcp.h
@@ -240,7 +240,8 @@ struct mtcp_manager
 
 	struct tcp_listener *listener;
 	TAILQ_HEAD(, mon_listener) monitors;
-	int num_msp;                    /* # of MOS_SOCK_MONITOR_STREAM */
+	uint32_t num_msp;               /* # of MOS_SOCK_MONITOR_STREAM */
+	uint32_t num_esp;               /* # of MOS_SOCK_STREAM */
 	struct pkt_ctx *pctx;			/* current pkt context */
 
 	stream_queue_t connectq;				/* streams need to connect */

core/src/include/mtcp_api.h
@@ -117,6 +117,8 @@ struct app_ops
 	/** Function contains code which will be executed after initialization
 	 * phase */
 	void (*run)(mctx_t mctx, void **app_ctx);
+	/* is the application enabled at the moment? */
+	int enable;
 };
 
 
@@ -382,7 +384,7 @@ mtcp_recv(mctx_t mctx, int sockid, char *buf, size_t len, int flags);
 /** Read byte stream from flow, same as `read()`
  * @return number of bytes read on success, -1 on error
  */
-inline ssize_t
+ssize_t
 mtcp_read(mctx_t mctx, int sock, char *buf, size_t len);
 
 /* readv should work in atomic */

core/src/include/tcp_rb.h
@@ -98,6 +98,9 @@ tcprb_resize(tcprb_t *rb, int len);
 extern inline int
 tcprb_ffhead(tcprb_t *rb, int len);
 
+inline int
+tcprb_fflen(tcprb_t *rb, uint8_t *buf, int len, loff_t off);
+
 extern inline int
 tcprb_ppeek(tcprb_t *rb, uint8_t *buf, int len, loff_t off);
 

core/src/ip_in.c
@@ -44,11 +44,6 @@ ProcessInIPv4Packet(mtcp_manager_t mtcp, struct pkt_ctx *pctx)
 		goto __return;
 	}
 
-	if (ip_fast_csum(iph, iph->ihl)) {
-		ret = ERROR;
-		goto __return;
-	}
-
 	if (iph->version != IPVERSION ) {
 		release = true;
 		ret = FALSE;
@@ -56,6 +51,30 @@ ProcessInIPv4Packet(mtcp_manager_t mtcp, struct pkt_ctx *pctx)
 	}
 
 	FillInPacketIPContext(pctx, iph, ip_len);
+
+	/* callback for monitor raw socket */
+	TAILQ_FOREACH(walk, &mtcp->monitors, link)
+		if (walk->socket->socktype == MOS_SOCK_MONITOR_RAW) {
+			if (ISSET_BPFFILTER(walk->raw_pkt_fcode) &&
+				EVAL_BPFFILTER(walk->raw_pkt_fcode, (uint8_t *)pctx->p.ethh,
+							   pctx->p.eth_len))
+				HandleCallback(mtcp, MOS_NULL, walk->socket, MOS_SIDE_BOTH,
+							   pctx, MOS_ON_PKT_IN);
+		}
+
+	/* if there is no MOS_SOCK_STREAM or MOS_SOCK_MONITOR_STREAM socket,
+	   forward IP packet before reaching upper (transport) layer */
+	if (mtcp->num_msp == 0 && mtcp->num_esp == 0) {
+		if (pctx->forward) {
+			ForwardIPPacket(mtcp, pctx);
+		}
+		return TRUE;
+	}		
+	
+	if (ip_fast_csum(iph, iph->ihl)) {
+		ret = ERROR;
+		goto __return;
+	}
 
 	switch (iph->protocol) {
 		case IPPROTO_TCP:
@@ -75,11 +94,6 @@ ProcessInIPv4Packet(mtcp_manager_t mtcp, struct pkt_ctx *pctx)
 	}
 
 __return:
-	/* callback for monitor raw socket */
-	TAILQ_FOREACH(walk, &mtcp->monitors, link)
-		if (walk->socket->socktype == MOS_SOCK_MONITOR_RAW)
-			HandleCallback(mtcp, MOS_NULL, walk->socket, MOS_SIDE_BOTH,
-				       pctx, MOS_ON_PKT_IN);
 	if (release && mtcp->iom->release_pkt)
 		mtcp->iom->release_pkt(mtcp->ctx, pctx->p.in_ifidx,
 				       (unsigned char *)pctx->p.ethh, pctx->p.eth_len);

commit 35f6bf22ed9ec5a9544d5eaf2db66fa7c1519192

Fix issue introduced by 05e3289
I had a lot of lines in the NAT like
[translate_addr: 144] mtcp_getpeer() failed sock=61 side=1

In nat.c, MOS_SIDE_CLI has been changed by MOS_SIDE_BOTH, without
changing the length of the sockaddr len.
Actually, by luck up to this patch the size of a pointer * 2 was equal
to the size of a sockaddr, but now the error in the sizeof appears.

I suspect this also happens in midstat.c, but I don't have time to push
this further.

samples/nat/nat.c
@@ -60,7 +60,7 @@ assign_port(mctx_t mctx, int sock)
 {
 	struct port *w;
 	struct sockaddr_in addr[2];
-	socklen_t len = sizeof(addr) * 2;
+	socklen_t len = sizeof(struct sockaddr_in) * 2;
 
 	/* remove a NAT mapping for this connection */
 	if (mtcp_getpeername(mctx, sock, (struct sockaddr *)&addr, &len,
@@ -137,7 +137,7 @@ translate_addr(mctx_t mctx, int sock, int side, uint64_t events,
 	} else /* if (side == MOS_SIDE_SVR) */ {
 		/* SVR (WAN) ==> CLI (LAN) : DNAT */
 		struct sockaddr_in addr[2];
-		socklen_t len = sizeof(addr) * 2;
+		socklen_t len = sizeof(struct sockaddr_in) * 2;
 
 		if (mtcp_getpeername(mctx, sock, (struct sockaddr *)&addr, &len,
 					MOS_SIDE_BOTH) < 0) {